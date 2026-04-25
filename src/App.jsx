9d5c842aac16d223f9f4a233f531646d40e8d02b
Your New Username <ycrijnaard@gmail.com>
2026-04-25 23:46:48 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <main className="h-screen w-screen flex flex-col items-center justify-center">
+      <h1 className="text-4xl">Kanban Board</h1>
+    </main>
+  );
+}
+
+export default App;
